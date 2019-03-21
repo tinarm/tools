5a2485f324c573f5897fbf0d2de993854c0cffef
Martin Navne <martin.navne@telavox.se>
2019-03-21 16:39:39 +0100

    Add linux .bashrc and java 8/11 setup scripts

---
+++ bin/set-java-11.sh
@@ -0,0 +1,3 @@
+sudo update-java-alternatives -s java-11-oracle
+export JAVA_HOME=/usr/lib/jvm/java-11-oracle/
+export PATH=$PATH:$JAVA_HOME
---
+++ bin/set-java-8.sh
@@ -0,0 +1,3 @@
+sudo update-java-alternatives -s java-8-oracle
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle/
+export PATH=$PATH:$JAVA_HOME
